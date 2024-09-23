677837e5229cb49fcbda203166c01d4f602a8d3c
nishantchy <nishantchy1234@gmail.com>
2024-09-23 15:54:20 +0545

    again

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
+// publicRoutes: ["/", "/forget-password", "/api/webhooks/clerk"];
 
 export const config = {
   matcher: [
